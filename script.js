bdbd72b5d30999f0656b0f1ae8d7bec0dc37149b
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-10-08 18:18:54 +0500

    added more

--- script.js
+++ script.js
@@ -4,9 +4,34 @@ const container = document.querySelector('.container');
 let searchQuery = '';
 const APP_ID = 'ea848459';
 const APP_KEY = '990a5a530df661c88b7c41d3b1442db3';
-const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
     searchQuery = e.target.querySelector('input').value;
-})
+    fetchAPI();
+});
+
+async function fetchAPI() {
+    const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}&to=15`;
+    const response = await fetch(baseURL);
+    const data = await response.json();
+    generateHTML(data.hits);
+    console.log(data);
+}
+
+function generateHTML(results) {
+    let generatedHTML = '';
+    results.map(result => {
+        generatedHTML += `
+                <div class="item">
+					<img src="${result.recipe.image}">
+					<div class="flex-container">
+						<h1 class="title">${result.recipe.label}</h1>
+						<a class="view-button" href="#">View Recipe</a>
+					</div>
+					<p class="item-data">Calories: ${result.recipe.calories}</p>
+				</div>
+        `
+    })
+    searchResultDiv.innerHTML = generatedHTML;
+}
