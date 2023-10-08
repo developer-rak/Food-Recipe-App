f8c7213f9bff8cdf1c5da452a599b28891f089f0
Rashid-Ali-Kalhoro <87558577+Rashid-Ali-Kalhoro@users.noreply.github.com>
2023-10-08 17:39:35 +0500

    added api

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const searchForm = document.querySelector('form');
 const searchResultDiv = document.querySelector('.search-result');
 const container = document.querySelector('.container');
 let searchQuery = '';
+const APP_ID = 'ea848459';
+const APP_KEY = '990a5a530df661c88b7c41d3b1442db3';
+const baseURL = `https://api.edamam.com/search?q=pizza&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
 searchForm.addEventListener('submit', (e) => {
     e.preventDefault();
